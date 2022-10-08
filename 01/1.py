be966dc72833690c094d45651cdb35c1e7b6804d
Kosenkov <9588434@gmail.com>
2022-10-08 21:02:50 +0300

    Task-01 was created

--- 01/1.py
+++ 01/1.py
@@ -2,4 +2,4 @@
 # обозначающую день недели, и проверяет, является ли этот день выходным.
 
 n = int(input('Input day number: '))
-print('Wrong day number' if 7 < n or n < 1 else 'Yes' if n == 6 or n == 7 else 'No')
+print('Wrong day number ' if 7 < n or n < 1 else 'Yes' if n == 6 or n == 7 else 'No')
